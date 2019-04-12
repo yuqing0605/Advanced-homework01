861e9c28b392d39430c420230b84c0a1012cdcd1
yuqing0605 <48577102+yuqing0605@users.noreply.github.com>
2019-04-12 11:29:32 +0800

    Add files via upload

---
+++ 進階1.cpp
@@ -0,0 +1,18 @@
+//3A713221
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int i=65;
+	
+	for( int i=65 ; i<=90 ; i++ ){
+		
+	printf("%c ",i);
+	printf("%c ",i+32);
+	}
+	
+	system("pause");
+	return 0;
+}
